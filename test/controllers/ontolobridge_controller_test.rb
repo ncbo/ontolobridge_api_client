ce2714c1b10fcd23bdc181abf5801478013b9563
mdorf <mdorf@stanford.edu>
2018-02-02 15:21:57 -0800

    added basic unit tests

--- test/controllers/ontolobridge_controller_test.rb
+++ test/controllers/ontolobridge_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class OntolobridgeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "term status retrieval" do
+    params = { term_id: 4, request_method: "post" }
+    post "/term_status", params: params
+    assert_response :success
+    params[:request_method] = "get"
+    post "/term_status", params: params
+    assert_response :success
+  end
+
+  test "term creation" do
+    params = { label: "Test Class 11", description: "Unit test dedicated class", superclass: "http://dev3.ccs.miami.edu:8080/ontolobridge#ONTB_000000008" }
+    post "/request_term", params: params
+    assert_response :success
+  end
+
 end
